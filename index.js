07304fcf210522fe9fbefc74ad36f51093a45da6
Claudio Spizzi <claudio@spizzi.net>
2019-04-07 12:23:25 +0200

    Add feature to retreive station values every 10 minutes

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 #!/usr/bin/env node
+
+const log = require('yalm');
+const mqtt = require('mqtt');
+
+const swissmeteo = require('./modules/swissmeteo.js');
+
+const pkg = require('./package.json');
+const cfg = require(process.argv[2] || './config.json');
+
+let mqttConnected;
+let weatherDataTime;
+
+log.setLevel(cfg.log);
+log.info(pkg.name + ' ' + pkg.version + ' starting');
+
+const mqttClient = mqtt.connect(
+    cfg.mqtt.url, {
+        will: { topic: cfg.mqtt.name + '/connected', payload: '0', retain: true },
+        rejectUnauthorized: cfg.mqtt.secure
+    }
+);
+
+mqttClient.on('connect', () => {
+
+    mqttClient.publish(cfg.mqtt.name + '/connected', '2', { retain: true });
+
+    mqttConnected = true;
+    log.info('mqtt: connected ' + cfg.mqtt.url);
+});
+
+mqttClient.on('close', () => {
+
+    if (mqttConnected) {
+        mqttConnected = false;
+        log.info('mqtt: disconnected ' + cfg.mqtt.url);
+    }
+});
+
+mqttClient.on('error', err => {
+
+    log.error('mqtt: error ' + err.message);
+});
+
+function pollSwissMeteoData() {
+
+    swissmeteo.getWeatherData(cfg.swissmeteo.weatherStation.id, (error, response) => {
+        if (error) {
+            log.error(error);
+        }
+        else {
+            log.debug('swissmeteo weather data: ' + JSON.stringify(response));
+            if (weatherDataTime != response.timestamp) {
+                properties = [ 'temperature', 'precipitation', 'sunshine', 'radiation', 'humidity', 'dewPoint', 'windDirection', 'windSpeed', 'gustPeak', 'pressure' ];
+                for (var key in properties) {
+                    var property = properties[key];
+                    if (response[property] != undefined) {
+                        var payload = JSON.stringify({ val: response[property], ts: response.timestamp });
+                        mqttClient.publish(cfg.mqtt.name + '/' + property + '/' + cfg.swissmeteo.weatherStation.name, payload);
+                        log.info('mqtt: publish ' + cfg.mqtt.name + '/' + property + '/' + cfg.swissmeteo.weatherStation.name + ' ' + payload);
+                    }
+                }
+                weatherDataTime = response.timestamp;
+            }
+        }
+    });
+}
+
+setInterval(pollSwissMeteoData, cfg.swissmeteo.pollInterval * 1000);
---
+++ modules/swissmeteo.js
@@ -0,0 +1,95 @@
+const request = require('request');
+const csvParse = require('csv-parse');
+const moment = require('moment');
+
+// Automatic Measurement Stations:
+// https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv
+// https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/info/VQHA80_en.txt
+
+// Precipitation Stations:
+// https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA98.csv
+// https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/info/VQHA98_en.txt
+
+function getWeatherData(station, callback) {
+
+    var url = 'https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv';
+    var header = 'MeteoSchweiz / MeteoSuisse / MeteoSvizzera / MeteoSwiss';
+
+    request.get(url, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+        }
+        else if (response.statusCode != 200) {
+            callback('Request failed with http status code ' + response.statusCode, null);
+        }
+        else {
+            var allData = body.replace(header, '').trim();
+            csvParse(allData, {
+                delimiter: ';',
+                columns: true
+            }, (error, output) => {
+                if (error) {
+                    callback(error, null);
+                }
+                else {
+                    for (var key in output) {
+                        if (output[key].stn == station) {
+                            callback(null, parseWeatherData(output[key]));
+                        }
+                    }
+                }
+            });
+        }
+    });
+}
+
+function parseWeatherData(data) {
+
+    var parser = {
+        tre200s0: {
+            field: "temperature"
+        },
+        rre150z0: {
+            field: "precipitation"
+        },
+        sre000z0: {
+            field: "sunshine"
+        },
+        gre000z0: {
+            field: "radiation"
+        },
+        ure200s0: {
+            field: "humidity"
+        },
+        tde200s0: {
+            field: "dewPoint"
+        },
+        dkl010z0: {
+            field: "windDirection"
+        },
+        fu3010z0: {
+            field: "windSpeed"
+        },
+        fu3010z1: {
+            field: "gustPeak"
+        },
+        prestas0: {
+            field: "pressure"
+        }
+    };
+
+    var weatherData = {
+        station: data.stn,
+        timestamp: moment.utc(data.time, 'YYYYMMDDHHmm').format('x')
+    };
+
+    for (var key in parser) {
+        if (data[key] != '-') {
+            weatherData[parser[key].field] = Number(data[key]);
+        }
+    }
+
+    return weatherData;
+}
+
+module.exports.getWeatherData = getWeatherData;
