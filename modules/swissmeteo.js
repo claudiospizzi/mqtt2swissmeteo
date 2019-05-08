a317422892f214ad5122fe6cf16dbeda6385210f
Claudio Spizzi <claudio@spizzi.net>
2019-05-08 13:14:19 +0200

    Update swissmeteo module, simplify structure

--- modules/swissmeteo.js
+++ modules/swissmeteo.js
@@ -1,19 +1,29 @@
-// API Description
-// - Automatic Measurement Stations:
-//   https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv
-//   https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/info/VQHA80_en.txt
-// - Precipitation Stations:
-//   https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA98.csv
-//   https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/info/VQHA98_en.txt
-
 const request = require('request');
 const csvParse = require('csv-parse');
 const moment = require('moment');
 
+/**
+ * Get the MeteoSwiss weather data of the specified station.
+ * @param {string}                   station  Weather station id.
+ * @param {function(string, string)} callback Callback with the response object.
+ */
 function getWeatherData(station, callback) {
 
     var url = 'https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv';
     var header = 'MeteoSchweiz / MeteoSuisse / MeteoSvizzera / MeteoSwiss';
+
+    var parser = {
+        tre200s0: { field: "temperature" },
+        rre150z0: { field: "precipitation" },
+        sre000z0: { field: "sunshine" },
+        gre000z0: { field: "radiation" },
+        ure200s0: { field: "humidity" },
+        tde200s0: { field: "dewPoint" },
+        dkl010z0: { field: "windDirection" },
+        fu3010z0: { field: "windSpeed" },
+        fu3010z1: { field: "gustPeak" },
+        prestas0: { field: "pressure" }
+    };
 
     request.get(url, (error, response, body) => {
         if (error) {
@@ -34,7 +44,19 @@ function getWeatherData(station, callback) {
                 else {
                     for (var key in output) {
                         if (output[key].stn == station) {
-                            callback(null, parseWeatherData(output[key]));
+
+                            var data = {
+                                station: output[key].stn,
+                                timestamp: moment.utc(output[key].time, 'YYYYMMDDHHmm').format('x')
+                            };
+
+                            for (var parserKey in parser) {
+                                if (output[key][parserKey] != '-') {
+                                    data[parser[parserKey].field] = Number(output[key][parserKey]);
+                                }
+                            }
+
+                            callback(null, data);
                         }
                     }
                 }
@@ -43,53 +65,5 @@ function getWeatherData(station, callback) {
     });
 }
 
-function parseWeatherData(data) {
-
-    var parser = {
-        tre200s0: {
-            field: "temperature"
-        },
-        rre150z0: {
-            field: "precipitation"
-        },
-        sre000z0: {
-            field: "sunshine"
-        },
-        gre000z0: {
-            field: "radiation"
-        },
-        ure200s0: {
-            field: "humidity"
-        },
-        tde200s0: {
-            field: "dewPoint"
-        },
-        dkl010z0: {
-            field: "windDirection"
-        },
-        fu3010z0: {
-            field: "windSpeed"
-        },
-        fu3010z1: {
-            field: "gustPeak"
-        },
-        prestas0: {
-            field: "pressure"
-        }
-    };
-
-    var weatherData = {
-        station: data.stn,
-        timestamp: moment.utc(data.time, 'YYYYMMDDHHmm').format('x')
-    };
-
-    for (var key in parser) {
-        if (data[key] != '-') {
-            weatherData[parser[key].field] = Number(data[key]);
-        }
-    }
-
-    return weatherData;
-}
-
+// Node.JS module function export
 module.exports.getWeatherData = getWeatherData;
